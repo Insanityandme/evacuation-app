2d10f5c52abdd0b874400d6e80e9a8f9343f24a2
Filip Bengtegård <bengtegardbook@gmail.com>
2023-03-22 14:43:05 +0100

    Created first draft of back-end

---
+++ backend/evacuation-app/src/main/java/com/evacuation/evacuationapp/EvacuationAppApplication.java
@@ -0,0 +1,13 @@
+package com.evacuation.evacuationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvacuationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvacuationAppApplication.class, args);
+    }
+
+}
---
+++ backend/evacuation-app/src/test/java/com/evacuation/evacuationapp/EvacuationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evacuation.evacuationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvacuationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

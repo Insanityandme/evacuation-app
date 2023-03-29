b120a2acb08b654c107b678a8cca578c0cba9990
Ellie Rosander <eli.rosander95@outlook.com>
2023-03-29 10:12:07 +0200

    maven backend
    maven backend början (model + pom fixad)

---
+++ backend/springbootsecuritypostgresql/src/main/java/com/example/springbootsecuritypostgresql/SpringbootsecuritypostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritypostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecuritypostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsecuritypostgresqlApplication.class, args);
+    }
+
+}

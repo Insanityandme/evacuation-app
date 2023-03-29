8dbc70095aeb3aeead7823e0d166dd60d429e8e1
Ellie Rosander <eli.rosander95@outlook.com>
2023-03-29 11:02:56 +0200

    UPPDATERAT RÄTT MAP
    UPPDATERAT RÄTT MAP med MODEL PAKET

--- backend/spring-boot-security-postgresql/src/main/java/com/example/springbootsecuritypostgresql/SpringBootSecurityPostgresqlApplication.java
+++ backend/spring-boot-security-postgresql/src/main/java/com/example/springbootsecuritypostgresql/SpringBootSecurityPostgresqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootsecuritypostgresqlApplication {
+public class SpringBootSecurityPostgresqlApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootsecuritypostgresqlApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+	}
 
 }

954b4fbfdab6065e74e8f646d2d4df2bbbc53c43
Filip Bengtegård <bengtegardbook@gmail.com>
2023-04-07 10:00:18 +0200

    removed some files

--- backend/evacuation-app/src/main/java/com/evacuation/evacuationapp/entities/LoginDetailsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.evacuation.evacuationapp.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginDetailsRepository extends JpaRepository<LoginDetails, Long> {
-}

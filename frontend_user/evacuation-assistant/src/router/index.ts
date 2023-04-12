9aa9fdae9861491a36048937e32a48e809eb3b0b
Filip Bengtegård <bengtegardbook@gmail.com>
2023-04-12 18:37:26 +0200

    Testing JSON parsing to access user data

--- frontend_user/evacuation-assistant/src/router/index.ts
+++ frontend_user/evacuation-assistant/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import TabsPage from '../views/deputy_leader/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,22 +17,22 @@ const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'home',
-        component: () => import('@/views/HomePage.vue')
+        component: () => import('@/views/deputy_leader/HomePage.vue')
       },
       {
         path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        component: () => import('@/views/deputy_leader/Tab2Page.vue')
       },
       {
         path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        component: () => import('@/views/deputy_leader/Tab3Page.vue')
       }
     ]
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/LoginPage.vue')
+    component: () => import('@/views/deputy_leader/LoginPage.vue')
   },
 ]
 

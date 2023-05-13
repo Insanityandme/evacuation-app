4f5da3c68c5d2ba115f0a0b7e3e2772c54eadb8e
Al-Darraji-95 <86610523+Al-Darraji-95@users.noreply.github.com>
2023-05-13 19:47:01 +0200

    Cleaned up old code

--- frontend_admin/evacuation-admin/src/components/AddEditUserContainer.vue
+++ frontend_admin/evacuation-admin/src/components/AddEditUserContainer.vue
@@ -12,11 +12,6 @@
             <ion-item>
                 <ion-input type="password" :value="state.password" @input="state.password=$event.target.value" label="Enter Password" label-placement="floating" placeholder="********"/>
             </ion-item>
-
-            <!--<ion-item @click="openPicker()" class="clickable ion-focusable">
-                <ion-label>Choose Role and Responsibilities:</ion-label>
-                <ion-button button-type="submit" @click="openPicker()">{{state.role || 'Select'}}</ion-button>
-            </ion-item>-->
             <ion-item>
                 <ion-select
                     aria-label="Select Role"
@@ -69,9 +64,6 @@
             <ion-item v-if="state.role === 'other'">
                 <ion-input :value="state.username" @input="state.username=$event.target.value" label="Enter Role" label-placement="floating" placeholder="e.g. JohnDoe"/>
             </ion-item>
-            <ion-item lines="none">
-                <ion-label>Current value: {{ state.zone }}</ion-label>
-            </ion-item>
         </ion-list>
         <ion-button expand="block" shape="round" size="large" @click="submitForm()">Save</ion-button>
     </div>
@@ -113,143 +105,6 @@ const rules = {
 }
 const v$ = useVuelidate(rules, state);
 
-const openPicker = async () => {
-    const picker = await pickerController.create({
-        columns: [
-            {
-                name: 'role',
-                options: [
-                    {
-                        text: 'Select Role',
-                        value: 'select-role',
-                    },
-                    {
-                        text: 'Evacuation Leader',
-                        value: 'evac',
-                    },
-                    {
-                        text: 'Deputy Leader',
-                        value: 'deputy',
-                    },
-                    {
-                        text: 'Special Needs User',
-                        value: 'special-needs-user',
-                    },
-                ],
-            },
-            {
-                name: 'floor',
-                options: [
-                    {
-                        text: 'Select Floor',
-                        value: 'select-floor',
-                    },
-                    {
-                        text: '1',
-                        value: 'FIRST FLOOR',
-                    },
-                    {
-                        text: '2',
-                        value: 'SECOND FLOOR',
-                    },
-                    {
-                        text: '3',
-                        value: 'THIRD FLOOR',
-                    },
-                    {
-                        text: '4',
-                        value: 'FOURTH FLOOR',
-                    },
-                    {
-                        text: '5',
-                        value: 'FIFTH FLOOR',
-                    },
-                    {
-                        text: '6',
-                        value: 'SIXTH FLOOR',
-                    },
-                    {
-                        text: '7',
-                        value: 'SEVENTH FLOOR',
-                    },
-                    {
-                        text: '8',
-                        value: 'EIGHTH FLOOR',
-                    },
-                    {
-                        text: '9',
-                        value: 'NINTH FLOOR',
-                    },
-                    {
-                        text: '10',
-                        value: 'TENTH FLOOR',
-                    },
-                ],
-            },
-            {
-                name: 'zone',
-                options: [
-                    {
-                        text: 'Select Zone',
-                        value: 'select-zone',
-                    },
-                    {
-                        text: 'A',
-                        value: 'A',
-                    },
-                    {
-                        text: 'B',
-                        value: 'B',
-                    },
-                    {
-                        text: 'C',
-                        value: 'C',
-                    },
-                ],
-            },
-            {
-                name: 'priority',
-                options: [
-                    {
-                        text: 'Select Priority',
-                        value: 'select-priority',
-                    },
-                    {
-                        text: 'High',
-                        value: 1,
-                    },
-                    {
-                        text: 'Medium',
-                        value: 2,
-                    },
-                    {
-                        text: 'Low',
-                        value: 3,
-                    },
-                ],
-            },
-        ],
-        buttons: [
-            {
-                text: 'Cancel',
-                role: 'cancel',
-            },
-            {
-                text: 'Confirm',
-                role: 'confirm',
-                handler: (value:string) => {
-                    state.role = value.role.value;
-                    state.floorname = value.floor.value;
-                    state.zone = value.zone.value;
-                    state.priority = value.priority.value;
-                },
-            },
-        ],
-        mode: 'ios',
-        cssClass: 'my-picker',
-    });
-    await picker.present();
-}
 const submitForm = async () => {
     const isFormCorrect = await v$.value.$validate();
     console.log(state.username);

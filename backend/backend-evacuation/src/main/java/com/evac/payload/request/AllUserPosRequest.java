279fcb44a4f274a62ca3b978cea5bc0002e701f3
Ellie Rosander <eli.rosander95@outlook.com>
2023-05-19 13:36:42 +0200

    ändrat getAllUserPos
    returnerar floor+zone också nu

---
+++ backend/backend-evacuation/src/main/java/com/evac/payload/request/AllUserPosRequest.java
@@ -0,0 +1,74 @@
+package com.evac.payload.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@JsonSerialize
+public class AllUserPosRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String position;
+
+    @NotBlank
+    private LocalDateTime localDateTime;
+
+    @NotBlank
+    private String floorName;
+
+    @NotBlank
+    private String zoneName;
+
+    public AllUserPosRequest(String username, String sensorSetPos, LocalDateTime localDateTime, String floorName, String zoneName) {
+        this.username = username;
+        this.position = sensorSetPos;
+        this.localDateTime = localDateTime;
+        this.floorName = floorName;
+        this.zoneName = zoneName;
+    }
+    public AllUserPosRequest() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getFloorName() {
+        return floorName;
+    }
+
+    public void setFloorName(String floorName) {
+        this.floorName = floorName;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+}

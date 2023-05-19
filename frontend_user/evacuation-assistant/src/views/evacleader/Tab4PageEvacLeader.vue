a79ec61a0118b26c2399e641111d2296ca6eed08
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2023-05-19 15:28:34 +0200

    Fixed the label text next to the toggles in Settings. Not so pretty though

--- frontend_user/evacuation-assistant/src/views/evacleader/Tab4PageEvacLeader.vue
+++ frontend_user/evacuation-assistant/src/views/evacleader/Tab4PageEvacLeader.vue
@@ -17,20 +17,26 @@
             <ion-label>Assigned floor: 4 and zone: C</ion-label>
           </ion-item>
 
-          <ion-item>
-            <ion-label>Please choose your settings.</ion-label>
-          </ion-item>
+          <ion-toolbar>
+            <ion-header>
+              <ion-title color="default">Choose your settings</ion-title>
+            </ion-header>
+          </ion-toolbar>
+
         </ion-toolbar>
 
       <ion-list>
         <ion-item>
-          <ion-toggle labelPlacement="end">Receive Push Notifications</ion-toggle>
+          <ion-toggle></ion-toggle>
+          <ion-label>  Receive Push Notifications</ion-label>
         </ion-item>
         <ion-item>
-          <ion-toggle labelPlacement="end">Receive Emails</ion-toggle>
+          <ion-toggle></ion-toggle>
+          <ion-label>  Receive Emails</ion-label>
         </ion-item>
         <ion-item>
-          <ion-toggle labelPlacement="end">Receive Text Messages</ion-toggle>
+          <ion-toggle></ion-toggle>
+          <ion-label>  Receive Text Messages</ion-label>
         </ion-item>
       </ion-list>
 

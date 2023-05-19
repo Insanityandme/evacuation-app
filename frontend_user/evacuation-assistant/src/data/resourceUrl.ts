8c9d162c70bfa80379992a89562600f3dade65a3
Arturo Bendezu <artycok123@gmail.com>
2023-05-19 13:09:35 +0200

    Commented a part of code that will be implemented in another tab

--- frontend_user/evacuation-assistant/src/data/resourceUrl.ts
+++ frontend_user/evacuation-assistant/src/data/resourceUrl.ts
@@ -1,1 +1,1 @@
-export const resourceUrl = 'http://192.168.1.135:8081';
+export const resourceUrl = 'http://10.2.4.45:8081'; //This will be frequently changed depending on the network
--- frontend_user/evacuation-assistant/src/views/deputyleader/Tab2PageDeputyLeader.vue
+++ frontend_user/evacuation-assistant/src/views/deputyleader/Tab2PageDeputyLeader.vue
@@ -19,42 +19,44 @@
             </ion-header>
 
 
-            <div id="incoming">
-                <ion-card>
-                    <ion-card-header>
-                        <ion-card-subtitle>Need of assistans</ion-card-subtitle>
-                        <ion-card-title>Please help</ion-card-title>
-                    </ion-card-header>
-
-                    <ion-card-content>
-                        A person in a wheel chair and with severe anxiety is stuck on Floor 4, Zone B.
-                        Can you help?
-                    </ion-card-content>
-
-                    <ion-button fill="clear">Sure, I'll help</ion-button>
-                    <ion-button fill="clear">Nah</ion-button>
-                </ion-card>
-            </div>
-
-            <div id="button">
-                <ion-item>
-                    <ion-button color="danger">Report hazard</ion-button>
-                </ion-item>
-                <ion-item>
-                    <ion-button color="dark">Not available</ion-button>
-                </ion-item>
-                <ion-item>
-                    <ion-button color="secondary">Help me</ion-button>
-                </ion-item>
-                <ion-item>
-                    <ion-button color="success">Done</ion-button>
-                </ion-item>
-            </div>
-
         </ion-content>
     </ion-page>
 </template>
 
+<!--
+
+<div id="incoming">
+<ion-card>
+  <ion-card-header>
+    <ion-card-subtitle>Need of assistans</ion-card-subtitle>
+    <ion-card-title>Please help</ion-card-title>
+  </ion-card-header>
+
+  <ion-card-content>
+    A person in a wheel chair and with severe anxiety is stuck on Floor 4, Zone B.
+    Can you help?
+  </ion-card-content>
+
+  <ion-button fill="clear">Sure, I'll help</ion-button>
+  <ion-button fill="clear">Nah</ion-button>
+</ion-card>
+</div>
+
+<div id="button">
+<ion-item>
+  <ion-button color="danger">Report hazard</ion-button>
+</ion-item>
+<ion-item>
+  <ion-button color="dark">Not available</ion-button>
+</ion-item>
+<ion-item>
+  <ion-button color="secondary">Help me</ion-button>
+</ion-item>
+<ion-item>
+  <ion-button color="success">Done</ion-button>
+</ion-item>
+</div>
+-->
 <script setup lang="ts">
 import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/vue';
 </script>

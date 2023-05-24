5faf4b97620763e54558025ac918460bb9958f1c
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2023-05-24 21:10:21 +0200

    Chenged the checklist and report-buttons to accordions

--- frontend_user/evacuation-assistant/src/data/resourceUrl.ts
+++ frontend_user/evacuation-assistant/src/data/resourceUrl.ts
@@ -1,2 +1,2 @@
 // This will be frequently changed depending on the network
-export const resourceUrl = 'http://192.168.0.72:8081'; //This will be frequently changed depending on the network
+export const resourceUrl = 'http://localhost:8081'; //This will be frequently changed depending on the network

0a003906ca8c0fccb741bab0ee6cb7bcd99fce57
Filip Bengtegård <bengtegardbook@gmail.com>
2023-05-29 13:43:40 +0200

    Update LoginForm.vue

--- frontend_user/evacuation-assistant/src/components/LoginForm.vue
+++ frontend_user/evacuation-assistant/src/components/LoginForm.vue
@@ -1,20 +1,21 @@
 <template>
-  <ion-list>
-    <ion-item>
-      <ion-input placeholder="Username" v-model="v$.username.$model" @blur="v$.username.$touch"></ion-input>
-      <ion-label color="danger" v-if="v$.username.$error">Name is required</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-input placeholder="Email" v-model="v$.email.$model" type="email"></ion-input>
-      <ion-label color="danger" v-if="v$.email.$error">Invalid email</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-input placeholder="Password" v-model="v$.password.$model" type="password"></ion-input>
-      <ion-label color="danger" v-if="v$.password.$error">Password is required</ion-label>
-    </ion-item>
-  </ion-list>
-  <ion-button class="ion-margin-top" color="primary" expand="block" @click="submitForm()">Sign in</ion-button>
-  <ion-button class="ion-margin-top" size="small" fill="clear" expand="block">Forgot your password?</ion-button>
+    <ion-list>
+        <ion-item>
+            <ion-input placeholder="Username" v-model="v$.username.$model"
+                       @blur="v$.username.$touch"></ion-input>
+            <ion-label color="danger" v-if="v$.username.$error">Name is required</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-input placeholder="Email" v-model="v$.email.$model" type="email"></ion-input>
+            <ion-label color="danger" v-if="v$.email.$error">Invalid email</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-input placeholder="Password" v-model="v$.password.$model" type="password"></ion-input>
+            <ion-label color="danger" v-if="v$.password.$error">Password is required</ion-label>
+        </ion-item>
+    </ion-list>
+    <ion-button class="ion-margin-top" color="primary" expand="block" @click="submitForm()">Sign in</ion-button>
+    <ion-button class="ion-margin-top" size="small" fill="clear" expand="block">Forgot your password?</ion-button>
 </template>
 
 <script setup lang="ts">
@@ -29,18 +30,18 @@ import {required, email} from '@vuelidate/validators';
  * as soon as you start typing
  */
 const state = reactive({
-  username: '',
-  email: '',
-  password: ''
+    username: '',
+    email: '',
+    password: ''
 })
 
 /**
  * Rules for validation
  */
 const rules = {
-  username: {required},
-  email: {required, email},
-  password: {required},
+    username: {required},
+    email: {required, email},
+    password: {required},
 }
 
 /**
@@ -55,10 +56,10 @@ const v$ = useVuelidate(rules, state);
  * to LoginPage.vue
  */
 const props = defineProps({
-  signInUser: {
-    type: Function,
-    required: true
-  }
+    signInUser: {
+        type: Function,
+        required: true
+    }
 })
 
 /**
@@ -67,11 +68,11 @@ const props = defineProps({
  * the data is in our backend database.
  */
 const submitForm = async () => {
-  const isFormCorrect = await v$.value.$validate();
+    const isFormCorrect = await v$.value.$validate();
 
-  if (isFormCorrect) {
-    props.signInUser({username: state.username, email: state.email, password: state.password});
-  }
+    if (isFormCorrect) {
+        props.signInUser({username: state.username, email: state.email, password: state.password});
+    }
 }
 </script>
 
